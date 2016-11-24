24df8497dc1fcbff0f219d1d29d85723d3fec475
tahsynx <tahsynx@gmail.com>
2016-11-25 00:47:38 +0600

    added reginstration insert.php, updated register.html

---
+++ reg_insert.php
@@ -0,0 +1,25 @@
+<?php
+$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
+$db = mysql_select_db("blood_bank", $connection); // Selecting Database from Server
+if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+$password = $_POST['password'];
+$sex = $_POST['sex'];
+$area = $_POST['area'];
+$blood_group = $_POST['blood_group'];
+$last_donation_date = $_POST['last_donation_date'];
+
+if($name !=''||$email !=''){
+//Insert Query of SQL
+$query = mysql_query("insert into donor(name, email, telephone, password, sex, area, blood_group, last_donation_date) values ('$name', '$email', '$telephone', '$password', '$sex', '$area', '$blood_group', '$last_donatoin_date')");
+echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+}
+else{
+echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
+}
+}
+mysql_close($connection); // Closing Connection with Server
+?>
+
